7e0fada370273d4977ddbb269ae3c0dab4e714b9
jmcol <jeff.colfer@gmail.com>
2017-01-25 20:31:51 -0500

    progress

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -38,7 +38,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Update epsilon using a decay function of your choice
-        self.epsilon = 1/(self.total_trials**2)
+        self.epsilon = self.epsilon - 0.01
 
         # Update additional class parameters as needed
         self.total_trials += 1
@@ -63,7 +63,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set 'state' as a tuple of relevant data for the agent
-        state = (inputs['light'], inputs['oncoming'], inputs['left'], inputs['right'], waypoint, deadline)
+        state = (inputs['light'], inputs['oncoming'], waypoint)
 
         return state
 
@@ -138,7 +138,7 @@ class LearningAgent(Agent):
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
 
         if self.learning:
-            self.Q[state][action] = (1 - self.alpha) * self.get_maxQ(state) + (reward * self.alpha)
+            self.Q[state][action] = (1 - self.alpha) * self.Q[state][action] + (reward * self.alpha)
 
         return
 
@@ -157,3 +157,48 @@ class LearningAgent(Agent):
         return
 
 
+def run():
+    """ Driving function for running the simulation.
+        Press ESC to close the simulation, or [SPACE] to pause the simulation. """
+
+    ##############
+    # Create the environment
+    # Flags:
+    #   verbose     - set to True to display additional output from the simulation
+    #   num_dummies - discrete number of dummy agents in the environment, default is 100
+    #   grid_size   - discrete number of intersections (columns, rows), default is (8, 6)
+    env = Environment()
+
+    ##############
+    # Create the driving agent
+    # Flags:
+    #   learning   - set to True to force the driving agent to use Q-learning
+    #    * epsilon - continuous value for the exploration factor, default is 1
+    #    * alpha   - continuous value for the learning rate, default is 0.5
+    agent = env.create_agent(LearningAgent, learning=True, alpha=0.95, epsilon=0.9)
+
+    ##############
+    # Follow the driving agent
+    # Flags:
+    #   enforce_deadline - set to True to enforce a deadline metric
+    env.set_primary_agent(agent, enforce_deadline=True)
+
+    ##############
+    # Create the simulation
+    # Flags:
+    #   update_delay - continuous time (in seconds) between actions, default is 2.0 seconds
+    #   display      - set to False to disable the GUI if PyGame is enabled
+    #   log_metrics  - set to True to log trial and simulation results to /logs
+    #   optimized    - set to True to change the default log file name
+    sim = Simulator(env, update_delay=0.01, display=False, log_metrics=True, optimized=True)
+
+    ##############
+    # Run the simulator
+    # Flags:
+    #   tolerance  - epsilon tolerance before beginning testing, default is 0.05
+    #   n_test     - discrete number of testing trials to perform, default is 0
+    sim.run(n_test=50, tolerance=0.01)
+
+
+if __name__ == '__main__':
+    run()
